cd973c04ce8d5c90d8ace41c59e5b2c23c37f97b
sudheer-g <bharadwaj.sudheer@gmail.com>
2018-01-22 19:13:55 +0530

    Client and server connection OK

--- src/main/java/com/work/chatapp/Server.java
+++ src/main/java/com/work/chatapp/Server.java
@@ -1,5 +1,48 @@
 package com.work.chatapp;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    public String test = "Hello World";
+    private ServerSocket serverSocket = null;
+    private Socket socket = null;
+    private DataInputStream in =  null;
+
+    public Server(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server listening on: " + port);
+            System.out.println("Waiting for a client ...");
+            socket = serverSocket.accept();
+            System.out.println("Client Accepted");
+            in = new DataInputStream(
+                    new BufferedInputStream(socket.getInputStream()));
+            // reads message from client until "Over" is sent
+            String line = "";
+            while (!line.equals("Over"))
+            {
+                try
+                {
+                    line = in.readUTF();
+                    System.out.println(line);
+                }
+                catch(IOException i)
+                {
+                    throw new RuntimeException(i);
+                }
+            }
+            System.out.println("Closing connection");
+
+            // close connection
+            socket.close();
+            in.close();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -1,10 +1,15 @@
+import com.work.chatapp.Client;
 import com.work.chatapp.Server;
 import org.testng.annotations.Test;
 
 public class ServerTest {
     @Test
     public void serverTest() {
-        Server server = new Server();
-        System.out.println(server.test);
+        Server server = new Server(5000);
+    }
+
+    @Test
+    public void clientTest() {
+        Client client = new Client("127.0.0.1", 5000);
     }
 }
